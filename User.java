b65fdd6de7a99186909b7340b9759ce3b686aa29
royfalaye <114830899+royfalaye@users.noreply.github.com>
2024-06-11 14:41:32 +0100

    Add files via upload

---
+++ User.java
@@ -0,0 +1,17 @@
+
+public class User {
+    String name;
+    String password;
+
+    void login() {
+        System.out.println("Logging");
+    }
+
+    String register() {
+        if (name.equals("") || password.equals("")) {
+            return "Invalid Login";
+        } else {
+            return "Success";
+        }
+    }
+}
