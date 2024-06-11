eebab528ecc2045ffb6b5a77a7129ae25e38c9d3
royfalaye <114830899+royfalaye@users.noreply.github.com>
2024-06-11 15:07:07 +0100

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,17 @@
+
+package Java2;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Daniel";
+        boolean alive = true;
+        if (alive == true){
+            System.out.println(name);
+        }
+        else{
+            System.out.println("Nothing to print");
+        }
+
+    }
+
+}
